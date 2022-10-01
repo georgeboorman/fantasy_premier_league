5b3c7cc26cd91c19bb5ae43454fb5bd9b78ca4ab
georgeboorman <61940102+georgeboorman@users.noreply.github.com>
2022-10-01 17:14:51 +0100

    Update fpl_app.py

--- fpl_app.py
+++ fpl_app.py
@@ -2,7 +2,6 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-from library import top_performers, player_performance
 
 # Set title
 st.title("Fantasy Premier League 20-21")
@@ -33,6 +32,28 @@ st.header("Best players by position")
 # Input field to choose position for plot
 position_selector = st.selectbox("Select position:", positions)
 
+# Function to return figure
+def top_performers(df, position, metric, num):
+    """
+    Function to return a bar plot of top X performing players by specified metric.
+    Args:
+    :param df: pandas DataFrame for use in function
+    :param position: Football position to be analyzed
+    :param metric: Statistic to be used for analysis
+    :param num: How many players to return
+    :return: fig: Figure to be plotted.
+    """
+
+    # Subset the data for the position of interest
+    data = grouped[grouped['position'] == position].sort_values(metric, ascending=False).head(num)
+
+    # Create the plot and update the layout
+    fig = px.bar(x='player', y=metric, data_frame=data, color='team',
+                title='Best {}s'.format(position_selector), labels={'total':'points'},
+                hover_data={'player':False, 'team':False})
+    fig.update_layout(xaxis_title='Player Name', yaxis_title='Fantasy Points')
+    return fig
+
 # Plot bar chart based on user input
 st.plotly_chart(top_performers(grouped, position_selector, 'total', 10))
 
@@ -41,6 +62,24 @@ st.header('Viewing individual player performance')
 
 # User input
 player_input = st.selectbox("Choose a player:", grouped['player'].sort_values())
+
+# Function to plot individual player performance across a season
+def player_performance(df, player):
+    """Function to return a scatter plot of an individual's performance across a season
+    Args:
+        df: pandas DataFrame for use in function
+        player: name of player to put in plot
+    Returns: figure to be plotted, total points"""
+
+    # Subset the data for the player of interest
+    data = df[df['player'] == player]
+
+    # Create the plot and update the layout
+    fig = px.scatter(data_frame=data, x='GW', y='total_points', color='opp_team', 
+                    title="{}'s Performance by Game Week".format(player),
+                    labels={'opp_team': 'Opponent', 'total_points':'Points', 'GW':'Game Week'})
+    fig.update_layout(xaxis_title='Game Week', yaxis_title='Fantasy Points', showlegend=False)
+    return fig
 
 # Plot scatter chart based on user input
 st.plotly_chart(player_performance(df, player_input))
